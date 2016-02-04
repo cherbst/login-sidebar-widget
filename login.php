d8bb14e7aa29c1901fecc77d1b315ecb66d1a8e8
Christoph Herbst <chris.p.herbst@gmail.com>
2016-02-04 15:11:19 +0100

    bump version to 5.2.2

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 Plugin Name: Login Widget With Shortcode
 Plugin URI: http://avifoujdar.wordpress.com/category/my-wp-plugins/
 Description: This is a simple login form in the widget. just install the plugin and add the login widget in the sidebar. Thats it. :)
-Version: 5.2.1-konnektiv
+Version: 5.2.2-konnektiv
 Author: avimegladon, konnektiv
 Author URI: http://avifoujdar.wordpress.com/
 */
